e38b0eb937f1edc9feb4bc2160b96a3a6c4a7f34
BryanHuet <53445609+BryanHuet@users.noreply.github.com>
2024-01-24 16:19:48 +0100

    Update and rename GeocodingController.java to MapController.java

--- src/main/java/com/project/newsmaper/MapController.java
+++ src/main/java/com/project/newsmaper/MapController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class GeocodingController {
+public class MapController {
 
     @GetMapping("/")
     public String home() {
